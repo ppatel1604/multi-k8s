077d3b7563ed10c343c0fac662271733d542b8fb
Parth Patel <p.patel81@yahoo.com>
2021-03-21 10:48:38 +1300

    🐛 Add space in the docker build for server image

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker build -t 160492/client:latest -t 160492/client:$SHA -f ./client/Dockerfile ./client
-docker build -t 160492/server:latest -t 160492/server:$SHA-f ./server/Dockerfile ./server
+docker build -t 160492/server:latest -t 160492/server:$SHA -f ./server/Dockerfile ./server
 docker build -t 160492/worker:latest -t 160492/worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push 160492/client:latest
